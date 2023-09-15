3b695dd7afe4765211fe36e2284dc4c756d7e1de
Masod Saidi <masod.s@gmail.com>
2023-09-16 00:09:13 +0200

    Fix logger type

--- MinimalApi.Endpoints.Tests/GetAccount.cs
+++ MinimalApi.Endpoints.Tests/GetAccount.cs
@@ -9,7 +9,7 @@ namespace MinimalApi.Endpoints.Tests
 	{
 		[EndpointGet("/accounts/{id}")]
 		[SwaggerOperation(Summary = "Get account by id")]
-		public async Task<IResult> GetAsync(int id, ILogger<WannaBeUserController> logger)
+		public async Task<IResult> GetAsync(int id, ILogger<GetAccount> logger)
 		{
 			logger.LogInformation("Accounts endpoint visited at {DT}", DateTime.UtcNow.ToLongTimeString());
 			return Results.Ok($"Getting id from accounts method endpoint {id}");
