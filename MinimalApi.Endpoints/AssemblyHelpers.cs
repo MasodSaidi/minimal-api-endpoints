675ce821a49afdb01c2404d808588b5a3d5b2766
Masod Saidi <masod.s@gmail.com>
2023-09-22 13:40:51 +0200

    Fix spacing

--- MinimalApi.Endpoints/AssemblyHelpers.cs
+++ MinimalApi.Endpoints/AssemblyHelpers.cs
@@ -2,32 +2,30 @@
 using MinimalApi.Attributes;
 using System.Reflection;
 
-namespace MinimalApi
+namespace MinimalApi;
+
+internal static class AssemblyHelpers
 {
-	internal static class AssemblyHelpers
-	{
-        internal static IEnumerable<Type> GetEndpointClasses()
-        {
-            var entryAssembly = Assembly.GetEntryAssembly();
-            if (entryAssembly == null)
-                throw new InvalidOperationException("Entry assembly not found");
+    internal static IEnumerable<Type> GetEndpointClasses()
+    {
+        var entryAssembly = Assembly.GetEntryAssembly();
+        if (entryAssembly == null)
+            throw new InvalidOperationException("Entry assembly not found");
 
-            return from type in entryAssembly.GetTypes()
-                   where type.IsClass && type.GetMethods().Any(m => m.IsDefined(typeof(EndpointBase), inherit: false))
-                   select type;
-        }
+        return from type in entryAssembly.GetTypes()
+                where type.IsClass && type.GetMethods().Any(m => m.IsDefined(typeof(EndpointBase), inherit: false))
+                select type;
+    }
 
-        internal static IEnumerable<MethodInfo> GetEndpointMethods()
-        {
-            var entryAssembly = Assembly.GetEntryAssembly();
-            if (entryAssembly == null)
-                throw new InvalidOperationException("Entry assembly not found");
+    internal static IEnumerable<MethodInfo> GetEndpointMethods()
+    {
+        var entryAssembly = Assembly.GetEntryAssembly();
+        if (entryAssembly == null)
+            throw new InvalidOperationException("Entry assembly not found");
 
-            return from type in entryAssembly.GetTypes()
-                   from method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
-                   where type.IsClass && method.IsDefined(typeof(EndpointBase), inherit: false)
-                   select method;
-        }
+        return from type in entryAssembly.GetTypes()
+                from method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                where type.IsClass && method.IsDefined(typeof(EndpointBase), inherit: false)
+                select method;
     }
 }
-
