7cf1492407733ee45521d83435e24bfe3c37dd59
Masod Saidi <masod.s@gmail.com>
2023-09-22 14:14:55 +0200

    Refactoring

--- MinimalApi.Endpoints/AssemblyHelpers.cs
+++ MinimalApi.Endpoints/AssemblyHelpers.cs
@@ -8,24 +8,29 @@ internal static class AssemblyHelpers
 {
     internal static IEnumerable<Type> GetEndpointClasses()
     {
-        var entryAssembly = Assembly.GetEntryAssembly();
-        if (entryAssembly == null)
-            throw new InvalidOperationException("Entry assembly not found");
+        var entryAssembly = GetEntryAssembly();
 
         return from type in entryAssembly.GetTypes()
-                where type.IsClass && type.GetMethods().Any(m => m.IsDefined(typeof(EndpointBase), inherit: false))
-                select type;
+               where type.IsClass && type.GetMethods().Any(m => m.IsDefined(typeof(EndpointBase), inherit: false))
+               select type;
     }
 
     internal static IEnumerable<MethodInfo> GetEndpointMethods()
     {
-        var entryAssembly = Assembly.GetEntryAssembly();
-        if (entryAssembly == null)
-            throw new InvalidOperationException("Entry assembly not found");
+        var entryAssembly = GetEntryAssembly();
 
         return from type in entryAssembly.GetTypes()
                 from method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
                 where type.IsClass && method.IsDefined(typeof(EndpointBase), inherit: false)
                 select method;
+    }
+
+    private static Assembly GetEntryAssembly()
+    {
+        var entryAssembly = Assembly.GetEntryAssembly();
+        if (entryAssembly == null)
+            throw new InvalidOperationException("Entry assembly not found");
+
+        return entryAssembly;
     }
 }
